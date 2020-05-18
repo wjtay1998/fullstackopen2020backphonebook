1ada70858d501690406df8fe3a869e6cac9e27a6
wjtay1998 <wjtay1998@gmail.com>
2020-05-18 18:46:16 +0800

    frontendbuild

---
+++ build/precache-manifest.888388b7cce4578e07e9ddc4d27fad5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9abf525c9a84ef41ece1ae3b1186f718",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccaf991dc3040865be4e",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a75cfe734ddb11b2e053",
+    "url": "/static/js/2.2381a9e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2381a9e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccaf991dc3040865be4e",
+    "url": "/static/js/main.391bdcbd.chunk.js"
+  },
+  {
+    "revision": "37e42bc47805ba865915",
+    "url": "/static/js/runtime-main.fac6ad05.js"
+  }
+]);
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 const customMorgan = morgan(':method :url :status :res[content-length] - :response-time ms :body')
 
 app.use(express.json())
+app.use(express.static('build'))
 app.use(customMorgan)
 app.use(cors())
 
