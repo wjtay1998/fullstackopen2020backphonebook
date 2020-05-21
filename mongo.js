30d4d7dd9dff046e6e3d1ddffa7e7ff851e38b8f
wjtay1998 <wjtay1998@gmail.com>
2020-05-21 16:11:31 +0800

    Connection with MongoDB

---
+++ mongo.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0-ht0p6.mongodb.net/test?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name: process.argv[3],
+  number: process.argv[4],
+})
+
+if(process.argv[3]){
+  person.save().then(result => {
+    console.log(`added ${result.name} number ${result.number} to phonebook`)
+    mongoose.connection.close()
+  })
+}else{
+  console.log('retrieving all entries')
+  Person.find({}).then(persons => { 
+    persons.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })  
+    mongoose.connection.close()
+  })
+}
